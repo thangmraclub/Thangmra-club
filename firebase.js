e439da287ef585fe7aa23efbd2b054444cf95334
thangmraclub <thangmraclub1@gmail.com>
2026-08-17 00:39:28 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 
 import {
   getAuth,
   signInWithEmailAndPassword,
   signOut
-} from
-"https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
 
 import {
   getDatabase,
@@ -15,21 +13,18 @@ import {
   set,
   remove,
   onValue
-} from
-"https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-database.js";
 
 
 const firebaseConfig = {
-
-  apiKey: "আপনার_apiKey",
-  authDomain: "আপনার_authDomain",
-  databaseURL: "আপনার_databaseURL",
+  apiKey: "AIzaSyDtnmDLlsd7ZCp8WAlk6VopTTmGEhrus6s",
+  authDomain: "thangmra-club.firebaseapp.com",
+  databaseURL: "https://thangmra-club-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "thangmra-club",
-  storageBucket: "আপনার_storageBucket",
-  messagingSenderId: "আপনার_messagingSenderId",
-  appId: "আপনার_appId",
-  measurementId: "আপনার_measurementId"
-
+  storageBucket: "thangmra-club.firebasestorage.app",
+  messagingSenderId: "245769120027",
+  appId: "1:245769120027:web:11df8fb2a45600b6254bbe",
+  measurementId: "G-2GV6JY7FPE"
 };
 
 
